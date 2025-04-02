9c5589e992e47dfd3b13e629744bbcd54e650341
Tadeg Quillien <tadeg.quillien@gmail.com>
2025-04-02 14:58:42 +0100

    init

--- src/Ending.jsx
+++ src/Ending.jsx
@@ -6,7 +6,7 @@ const Ending = () => {
     return (
         <div style={textStyle}>Thank you for your participation!
             <br></br>
-            Please click on this link to go back to Prolific: <a href="https://app.prolific.co/submissions/complete?cc=113C348D">https://app.prolific.co/submissions/complete?cc=113C348D</a>
+            Please click on this link to go back to Prolific: <a href="https://app.prolific.co/submissions/complete?cc=CZ96XAE8">https://app.prolific.co/submissions/complete?cc=CZ96XAE8</a>
             <br></br>
             After you have clicked the link, you can then close the present tab.</div>
     )
